dfc119b4c7d57c7e4ddc145584edd37c7cf5a97e
blckstkrl <comendadorkarl@gmail.com>
2025-12-14 17:24:02 +0800

    feat: landing page

---
+++ src/components/Hero.vue
@@ -0,0 +1,22 @@
+<template>
+    <div class="min-h-screen flex flex-col justify-center items-left pl-20" style="background-image: url('https://picsum.photos/1800/800?random=1');">
+
+    <div class="max-w-2xl">
+
+        <h1 class="text-5xl tracking-widest font-extrabold text-yellow-600 mb-5">ERRORIST</h1>
+        
+        <p class="text-lg mb-8 text-yellow-300 italic">
+            (n)someone who repeatedly makes
+            mistakes, or is always wrong.
+        </p>
+
+        <div class="flex flex-wrap gap-4">
+            <a href="#signup" class="bg-gray-500 text-black font-serif font-semibold px-7 py-4 rounded-xl shadow hover:bg-white hover:text-blue-900
+            transition">Be an Errorist</a>
+            <a href="#signup" class="bg-amber-400 text-black-600 font-serif font-semibold px-7 py-4 rounded-xl shadow hover:bg-white hover:text-blue-900
+            transition">Explore Course</a>
+        </div>
+    
+    </div>
+    </div>
+</template>
